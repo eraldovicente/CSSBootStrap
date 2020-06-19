02ef84524b3cad22dafe4a42722beaa95741e00b
Eraldo Vicente <eraldovicente2@gmail.com>
2020-06-19 00:02:25 -0300

    mostrando pagina atual

--- carolina_spa_com_php/templates/navegacion.php
+++ carolina_spa_com_php/templates/navegacion.php
@@ -10,19 +10,23 @@
         <div class="container">
             <div class="collapse navbar-collapse w-100" id="nav_principal">
                   <ul class="nav nav-justified w-100 flex-column flex-sm-row">
-                    <li class="nav-item">
+                    <?php 
+                        $archivo = basename( $_SERVER['PHP_SELF'] );
+                        $pagina = str_replace('.php', '', $archivo);
+                    ?>
+                    <li class="nav-item <?php echo 'index' === $pagina ? 'active' : '' ?>">
                         <a href="index.php" class="nav-link">Inicio</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item <?php echo 'nosotros' === $pagina ? 'active' : '' ?>">
                         <a href="nosotros.php" class="nav-link">Nosotros</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item <?php echo 'servicios' === $pagina ? 'active' : '' ?>">
                         <a href="servicios.php" class="nav-link">Servicios</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item <?php echo 'productos' === $pagina ? 'active' : '' ?>">
                         <a href="productos.php" class="nav-link">Produtos</a> 
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item <?php echo 'contacto' === $pagina ? 'active' : '' ?>">
                         <a href="contacto.php" class="nav-link">Contacto</a>
                     </li>
                   </ul>
